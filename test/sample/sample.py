249a0af207d0ae087913f12b653a004235a4e6cf
harjacober <harjacober@gmail.com>
2020-05-10 15:12:30 +1400

    added test folder

---
+++ test/sample/sample.py
@@ -0,0 +1,20 @@
+"""
+**
+ * author :   	Jacob
+ * created : 	Sat May  9 20:23:01 2020
+**
+"""
+"""
+**
+ * author :   	HP
+ * created : 	Sat May  9 20:23:01 2020
+**
+"""
+"""
+**
+ * author :   	Audu Jacob
+ * created : 	Sat May  9 20:23:01 2020
+**
+"""
+class Test:
+    pass
